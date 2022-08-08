6b88afc47a4babbfad30b6d641510dd1537db6e7
Diane Lopez <dianita956@users.noreply.github.com>
2022-08-08 15:17:12 -0500

    Update matamoros.py
    scripted NER and Parts of speech ES and keyword extraction.

--- matamoros.py
+++ matamoros.py
@@ -81,7 +81,6 @@ from IPython.display import Markdown, display
 import re
 
 def get_ner_in_context(keyword, document, desired_ner_labels= False):
-
     if desired_ner_labels != False:
         desired_ner_labels = desired_ner_labels
     else:
@@ -106,3 +105,103 @@ def get_ner_in_context(keyword, document, desired_ner_labels= False):
 
 for document in chunked_documents:
     get_ner_in_context('Mexico', document)
+
+# part of speech tagging for spanish
+# get POS tags
+for token in document:
+    print(token.lemma_, token.pos_, token.dep_)
+
+# get adjectives
+adjs = []
+for token in document:
+    if token.pos_ == 'ADJ':
+        adjs.append(token.lemma_)
+
+adjs_tally = Counter(adjs)
+adjs_tally.most_common()
+# make a list
+df = pd.DataFrame(adjs_tally.most_common(), columns=['adj', 'count'])
+df[:100]
+
+# get verbs
+verbs = [token.lemma_ for token in document if token.pos_ == 'VERB']
+
+verbs_tally = Counter(verbs)
+
+df = pd.DataFrame(verbs_tally.most_common(), columns=['verb', 'count'])
+df[:100]
+
+#keyword Extration- get sentences with keyword
+import re
+from IPython.display import Markdown, display
+
+def find_sentences_with_keyword(keyword, document):
+
+    #Iterate through all the sentences in the document and pull out the text of each sentence
+    for sentence in document.sents:
+        sentence = sentence.text
+
+        #Check to see if the keyword is in the sentence (and ignore capitalization by making both lowercase)
+        if keyword.lower() in sentence.lower():
+
+            #Use the regex library to replace linebreaks and to make the keyword bolded, again ignoring capitalization
+            sentence = re.sub('\n', ' ', sentence)
+            sentence = re.sub(f"{keyword}", f"**{keyword}**", sentence, flags=re.IGNORECASE)
+
+            display(Markdown(sentence))
+
+find_sentences_with_keyword(keyword="bueno", document=document)
+#get keyword in context
+#Make a list of tokens and POS labels from document if the token is a word
+tokens_and_labels = [(token.text, token.pos_) for token in document if token.is_alpha]
+#Make a function to get all two-word combinations
+def get_bigrams(word_list, number_consecutive_words=2):
+
+    ngrams = []
+    adj_length_of_word_list = len(word_list) - (number_consecutive_words - 1)
+
+    #Loop through numbers from 0 to the (slightly adjusted) length of your word list
+    for word_index in range(adj_length_of_word_list):
+
+        #Index the list at each number, grabbing the word at that number index as well as N number of words after it
+        ngram = word_list[word_index : word_index + number_consecutive_words]
+
+        #Append this word combo to the master list "ngrams"
+        ngrams.append(ngram)
+
+    return ngrams
+
+bigrams = get_bigrams(tokens_and_labels)
+bigrams[5:20]
+
+# get neighbor words
+def get_neighbor_words(keyword, bigrams, pos_label = None):
+
+    neighbor_words = []
+    keyword = keyword.lower()
+
+    for bigram in bigrams:
+
+        #Extract just the lowercased words (not the labels) for each bigram
+        words = [word.lower() for word, label in bigram]
+
+        #Check to see if keyword is in the bigram
+        if keyword in words:
+
+            for word, label in bigram:
+
+                #Now focus on the neighbor word, not the keyword
+                if word.lower() != keyword:
+                    #If the neighbor word matches the right pos_label, append it to the master list
+                    if label == pos_label or pos_label == None:
+                        neighbor_words.append(word.lower())
+
+    return Counter(neighbor_words).most_common()
+    get_neighbor_words("bien", bigrams)
+    get_neighbor_words("bien", bigrams, pos_label='VERB')
+
+    #find_sentences_with_keyword(keyword="YOUR KEY WORD", document=document)
+    #get_neighbor_words(keyword="YOUR KEY WORD", bigrams, pos_label=None)
+    #TF IDF term frequency inverse document frequency
+
+    #sentiment analysis
