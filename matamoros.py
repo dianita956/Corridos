9c2f756759fcd89a0021f6be78c2e53759575eec
Diane Lopez <dianita956@users.noreply.github.com>
2022-08-08 16:55:25 -0500

    run vader sentiment score

--- matamoros.py
+++ matamoros.py
@@ -207,9 +207,23 @@ def get_neighbor_words(keyword, bigrams, pos_label = None):
 #sentiment analysis
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 # Initialize VADER so we can use it later
-sentimentAnalyzer = SentimentIntensityAnalyzer()
+sid = SentimentIntensityAnalyzer()
 
 pd.options.display.max_colwidth = 400
 
-corridos_df = pd.read_csv(r'C:\Users\dmlpz\Corridos\Corridos.csv')
+corridos_filename = r'C:\Users\dmlpz\Corridos\Corridos.csv'
+
+corridos_df = pd.read_csv(r'C:\Users\dmlpz\Corridos\Corridos.csv', encoding='utf-8', engine='python')
 corridos_df.head()
+
+def calculate_sentiment(lyrics):
+    # Run VADER on the text
+    scores = sid.polarity_scores(text)
+    # Extract the compound score
+    compound_score = scores['compound']
+    # Return compound score
+    return compound_score
+
+# Apply the function to every row in the "text" column and output the results into a new column "sentiment_score"
+corridos_df['sentiment_score'] = corridos_df['lyrics'].apply(calculate_sentiment)
+corridos_df.sort_values(by='sentiment_score', ascending=False)[:10]
